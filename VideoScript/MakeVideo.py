07c0e7670e94376516d18a9f75f15c8cbf3c8304
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2017-04-20 23:10:18 +0100

    Modified the script to make the video to be more efficient.

--- VideoScript/MakeVideo.py
+++ VideoScript/MakeVideo.py
@@ -14,6 +14,9 @@
 #
 #============================================================================*/
 
+from datetime import datetime
+start_time = datetime.now()
+
 import numpy as np
 import matplotlib
 matplotlib.use("Agg")
@@ -31,22 +34,66 @@ Writer = animation.writers['ffmpeg']
 my_metadata = dict( title = 'Game of Life', artist = 'Carlo Sparaciari' )
 writer = Writer( fps = 10, metadata = my_metadata , bitrate = -1 )
 
+# Set the size of the figure to have 1 cell per dot
+my_dpi = 96
+my_width = float(config['number_x_cells'])/float(my_dpi)
+my_height = float(config['number_y_cells'])/float(my_dpi)
+if min( my_width, my_height ) < 10:
+    my_aspect_ratio = float(config['number_y_cells'])/float(config['number_x_cells'])
+    if my_width < my_height:
+        my_width = 10
+        my_height = my_width*my_aspect_ratio
+    else:
+        my_height = 10
+        my_width = my_height/my_aspect_ratio
+my_plot_size = (my_width, my_height)
+
+# Define set of frames we add into the video
+if 'initial_frame' in config:
+    start_frame = config['initial_frame']
+else:
+    start_frame = 0
+
+if 'final_frame' in config:
+    end_frame = config['final_frame']
+else:
+    end_frame = config['time_steps']
+
+# Configure the figure
+fig = plt.figure( figsize = my_plot_size, dpi = my_dpi )
+ax = plt.subplot(111)
+plt.axis('off')
+fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+ax.set_frame_on(False)
+plt.axis('off')
+centre_frame = np.array( [config['number_x_cells']/2, config['number_y_cells']/2] )
+
 # Load the input data and create frames
 video_frames = []
+after_config = datetime.now()
 
-fig = plt.figure()
-plt.axis('off')
 
-for iterator in range( config['time_steps'] ):
+print( ''.join( ['Time needed to configure the script : ', str(after_config - start_time)] ) )
+print( 'Start reading from files and create array of figures.' )
+for iterator in range( start_frame, end_frame ):
     frame = np.zeros( ( config['number_x_cells'] , config['number_y_cells'] ) , dtype = 'int' )
     file_name = ''.join( [config['file_name'], '_', str(iterator), '.', config['extension']] )
     alive_cells = np.loadtxt( os.path.join( my_path, 'Data' , file_name ), dtype = 'int' )
-    centre_array = np.full( (alive_cells.size/2, 2), np.array( [config['number_x_cells']/2, config['number_y_cells']/2] ), dtype = 'int' )
+    centre_array = np.full( (alive_cells.size/2, 2), centre_frame, dtype = 'int' )
     alive_cells = centre_array + alive_cells;
     frame[tuple(alive_cells.T)] = 1
-    video_frames.append([plt.imshow(frame)])
+    video_frames.append([plt.imshow(frame.T, aspect='auto')])
 
 # Create the video  
+after_reading = datetime.now()
+print( ''.join( ['Time needed to read files and create images : ', str(after_reading - after_config)] ) )
+print( 'Ready to create the video.' )
 my_animation = animation.ArtistAnimation( fig, video_frames, blit = True )
+after_video = datetime.now()
+print( ''.join( ['Time needed to create the video : ', str(after_video - after_reading)] ) )
+print( 'Video created.' )
 video_name = ''.join( [config['file_name'], '.mp4'] )
-my_animation.save( os.path.join( my_path, 'Video', video_name ), writer = writer )
+my_animation.save( os.path.join( my_path, 'Video', video_name ), dpi = my_dpi, writer = writer )
+after_save = datetime.now()
+print( ''.join( ['Time needed to save the video : ', str(after_save - after_video)] ) )
+print( 'Video saved.' )
--- VideoScript/README.txt
+++ VideoScript/README.txt
@@ -1,6 +1,13 @@
-Series of instruction for the example.
+With the script MakeVideo.py it is possible to make a movie out of the
+life files we produce with our command-line application golSerialApp.
 
-Starting from the build directory of the project:
+In what follows, we present the precise set of commands needed to create
+a video. The commands will produce a video of the well-known Gosper glider
+gun. You can download from LifeWiki a different pattern (the file format
+you need is the Life 1.06 one) and modify the config.dat file to specify
+the size of the grid and the number of steps.
+
+Starting from the build directory of the project, type the following:
 
 >  cp .\bin\golSerialApp.exe .\VideoScript\Data\
 >  cd .\VideoScript\Data\
@@ -10,4 +17,18 @@ Starting from the build directory of the project:
 >  python .\MakeVideo.py
 >  cd .\Video\
 
-In the floder ViseoScript\Video there is an mp4 video of the game.
+In the folder .\ViseoScript\Video there should be now an mp4 video called
+glide_evolution.mp4.
+
+NOTE: The script produces high-quality frames. Therefore, the size of the
+video can be large if the size of the frames is big or if the number of time
+steps is high. This might be a problem because in these situations, the script
+uses all the available RAM, and starts swapping (which sensibly decreases the
+preformances). If this is happening, you can modify the config.yaml file by
+inserting two additional field, initial_frame and final_frame. In this way,
+the script will produce a video starting from initial_frame and ending with
+final_frame (so that you can reduce the number of frames in a way that the
+script does not swap). Then, you can divide the video in several parts, and
+maybe unify them subsequently. This does improve the efficiency in the cases
+in which the script swaps.
+
